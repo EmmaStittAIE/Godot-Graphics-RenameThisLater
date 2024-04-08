4940cea678ab185f7ff80b4a0281b7c97beab93e
Emma Stitt <emmcooke@optusnet.com.au>
2024-04-08 17:04:18 +1000

    Another nondescript update

--- Scripts/Characters.cs
+++ Scripts/Characters.cs
@@ -11,6 +11,8 @@ public partial class Characters : Node3D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+        GD.Print("Woah bdfvkbdfkvbkbdfv");
+
         m_rotationTimer = m_timeToRotate;
     }
 
