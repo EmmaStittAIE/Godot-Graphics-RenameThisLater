1fa438208da8f86f91453845dff90f2153f73453
Emma Stitt <emmcooke@optusnet.com.au>
2024-04-29 17:07:39 +1000

    Made various changes based on feedback

--- Scripts/Characters.cs
+++ Scripts/Characters.cs
@@ -2,8 +2,12 @@ using Godot;
 
 public partial class Characters : Node3D
 {
-    private float m_currentRotationTarget = 0;
+    private float m_rotationOrigin = 0;
+    private float m_rotationTarget = 0;
     private float m_rotationTimer;
+
+    private float m_waveEffectTimer;
+    private bool m_waveEffectActive = false;
 
     private int m_currentTargetIndex = 0;
 
@@ -14,11 +18,14 @@ public partial class Characters : Node3D
 
     [Export]
     public float m_timeToRotate;
+    [Export]
+    public float m_waveEffectActivationSpeed;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
 	{
         m_rotationTimer = m_timeToRotate;
+        m_waveEffectTimer = m_waveEffectActivationSpeed;
     }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -26,17 +33,30 @@ public partial class Characters : Node3D
 	{
         if (m_rotationTimer < m_timeToRotate)
         {
-            float lerpedRot = Mathf.LerpAngle(Rotation.Y, m_currentRotationTarget, m_rotationTimer / m_timeToRotate);
-            RotateY(lerpedRot - Rotation.Y);
+            float lerpedRot = Mathf.Lerp(m_rotationOrigin, m_rotationTarget, 1 - Mathf.Pow(1 - (m_rotationTimer / m_timeToRotate), 8));
+            //RotateY(lerpedRot - Rotation.Y);
+            Rotation = new(Rotation.X, lerpedRot, Rotation.Z);
 
             m_rotationTimer += (float)delta;
+        }
+
+        if (m_waveEffectTimer < m_waveEffectActivationSpeed)
+        {
+            // Always a lerp between 0 and 1
+            float lerpedWaveIntensity = m_waveEffectTimer / m_waveEffectActivationSpeed;
+            if (!m_waveEffectActive) { lerpedWaveIntensity = 1 - lerpedWaveIntensity; }
+
+            RenderingServer.GlobalShaderParameterSet("waveFadeIn", lerpedWaveIntensity);
+
+            m_waveEffectTimer += (float)delta;
         }
 	}
 
     // Signal from LeftButton UI element
     private void OnLeftButtonPressed()
     {
-        m_currentRotationTarget += Mathf.Pi / 2;
+        m_rotationOrigin = Rotation.Y;
+        m_rotationTarget += Mathf.Pi / 2;
         m_rotationTimer = 0;
 
         m_animTrees[m_currentTargetIndex].Set("parameters/conditions/selected", false);
@@ -51,7 +71,8 @@ public partial class Characters : Node3D
     // Signal from RightButton UI element
     private void OnRightButtonPressed()
     {
-        m_currentRotationTarget -= Mathf.Pi / 2;
+        m_rotationOrigin = Rotation.Y;
+        m_rotationTarget -= Mathf.Pi / 2;
         m_rotationTimer = 0;
 
         m_animTrees[m_currentTargetIndex].Set("parameters/conditions/selected", false);
@@ -74,6 +95,7 @@ public partial class Characters : Node3D
     // Signal from WoahButton UI element
     private void OnWoahButtonToggle(bool toggledOn)
     {
-        RenderingServer.GlobalShaderParameterSet("waveEffect", toggledOn);
+        m_waveEffectActive = toggledOn;
+        m_waveEffectTimer = 0;
     }
 }
